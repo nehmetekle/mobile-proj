4eaed4e2bec80961769071a6dd914187bcedf70f
Nehme Tekle <nehmehabib@hotmail.com>
2023-12-10 18:13:18 +0200

    zabatna booking form

--- BookingForm.js
+++ BookingForm.js
@@ -3,13 +3,14 @@ import {
   View,
   Text,
   TouchableOpacity,
+  ScrollView,
   ImageBackground,
   StyleSheet,
 } from "react-native";
 import test from "./images/test.jpg";
 import { useNavigation } from "@react-navigation/native";
-import { useIsFocused } from "@react-navigation/native"; 
-
+import { useIsFocused } from "@react-navigation/native";
+ 
 import {
   getDatabase,
   get,
@@ -18,28 +19,28 @@ import {
 } from "firebase/database";
 import { db } from "./firebase";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+ 
 const ManageBooking = () => {
   const [userFlights, setUserFlights] = useState([]);
   const [user, setUser] = useState(null);
   const isFocused = useIsFocused();
-
+ 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const dbRef = ref(getDatabase());
-
+ 
         const snapshot = await get(child(dbRef, `booking_history`));
         if (snapshot.exists()) {
           const data = Object.values(snapshot.val());
-
+ 
           let newUserHistories = data.filter((data) => user && data.user_id === user.userId);
           let userFlights = [];
-
+ 
           const specialOffersSnapshot = await get(child(dbRef, `special_offers`));
           if (specialOffersSnapshot.exists()) {
             const specialData = Object.values(specialOffersSnapshot.val());
-
+ 
             newUserHistories.forEach((history) => {
               specialData.forEach((data) => {
                 if (data.id === history.offerId) {
@@ -54,14 +55,14 @@ const ManageBooking = () => {
               });
             });
           }
-
+ 
           setUserFlights(userFlights);
         }
       } catch (error) {
         console.error("Error fetching data:", error.message);
       }
     };
-
+ 
     const getUser = async () => {
       try {
         const jsonValue = await AsyncStorage.getItem('user');
@@ -74,19 +75,20 @@ const ManageBooking = () => {
         console.error("Error getting user:", e.message);
       }
     };
-
+ 
     getUser();
   }, [isFocused]);
-
+ 
   const navigation = useNavigation();
-
+ 
   return (
+    <ScrollView style={styles.container} contentContainerStyle={styles.scrollContainer}>
     <View style={styles.manageBookingContainer}>
       <ImageBackground source={test} style={styles.image}>
         <Text style={styles.heading}>Manage Booking</Text>
         <View style={styles.customLine} />
-
-        {/* Display booking data */}
+ 
+       
         {userFlights.map((booking, index) => (
           <View key={index} style={styles.bookingDetails}>
             <Text style={styles.bookingText}>Offer: {booking.data.offerName}</Text>
@@ -98,15 +100,23 @@ const ManageBooking = () => {
         ))}
       </ImageBackground>
     </View>
+    </ScrollView>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   manageBookingContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#ffffff",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  scrollContainer: {
+    flexGrow: 1,
   },
   heading: {
     fontWeight: "bold",
@@ -130,13 +140,16 @@ const styles = StyleSheet.create({
     marginTop: 80,
   },
   bookingDetails: {
-    marginTop: 20,
-    marginLeft: 20,
+    padding: 10,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
   },
   bookingText: {
     color: 'white',
     fontSize: 16,
   },
 });
-
+ 
 export default ManageBooking;
